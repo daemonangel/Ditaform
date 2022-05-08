cd4befe408d732adb6a17c37d51b03d3baa42742
daemonangel <daemonangel@users.noreply.github.com>
2022-05-08 18:26:54 -0400

    added comment in filedownloader about using MSAL to download from sharepoint

--- FileDownloader.cpp
+++ FileDownloader.cpp
@@ -12,6 +12,7 @@ FileDownloader::FileDownloader(QObject* parent)
     //request.setUrl(QUrl("https://zebra.sharepoint.com/:u:/s/ETP/EWQ-s0wzN-xAjIdYv70pPyUBdAn0wg5Wmsixpxjv2ANbZQ?e=Ktpc9J"));
     HRESULT hr = URLDownloadToFile(NULL, L"https://zebra.sharepoint.com/sites/ETP/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FETP%2FShared%20Documents%2FDITA%20Repository%2Fdita%2Dfiles%2Fbarcode%2Dtest%2Edita&parent=%2Fsites%2FETP%2FShared%20Documents%2FDITA%20Repository%2Fdita%2Dfiles&p=true&ga=1", L".\\PRAYFORDOWNLOAD.dita", 0, 0);
     //sharepoint with webview2
+    //sharepoint with https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-overview
     //public git repo
     //sharepoint using REST API and OAUTH
     //request.setRawHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0");
